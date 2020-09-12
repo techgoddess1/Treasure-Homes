230e0ac9e681774be9b9c89706b3332d6a6a4610
techgoddess1 <francesoparaocha@gmail.com>
2020-09-12 03:51:22 +0100

    add cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/api/v1', router);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, token');
