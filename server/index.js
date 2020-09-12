f133c716d0c32c25cbf178cf614f522075361a48
techgoddess1 <francesoparaocha@gmail.com>
2020-09-12 03:38:55 +0100

    add cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin', 'Content-Type', 'Authorization', 'token');
+  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, token');
 
   next();
 });
