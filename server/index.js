10912a44faaaf0bb1019f115eaa08668886518c4
techgoddess1 <francesoparaocha@gmail.com>
2020-09-12 04:03:48 +0100

    add cors

--- server/index.js
+++ server/index.js
@@ -1,22 +1,14 @@
 import express from 'express';
-
+import cors from 'cors';
 import router from './routes';
 import Response from './helpers/ResponseModel';
 
 const app = express();
+app.use(cors);
 
 app.use(express.json());
 
 app.use('/api/v1', router);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, token');
-
-  next();
-});
 
 app.use('*', (req, res) => {
   res.status(404).json(new Response(false, 404, 'You typed in the wrong URL'));
